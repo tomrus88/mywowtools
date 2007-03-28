52f3dc87c9fe6c1dc3f1151ba67e037a4755d53c
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2007-03-28 21:33:23 +0000

    - Uploaded a9_parser

---
+++ trunk/a9_parser/a9_parser/Flags.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace a9_parser
+{
+    public partial class Form1
+    {
+        [Flags]
+        public enum Flags
+        {
+            byte1 = 0x1,
+            byte2 = 0x2,
+            byte3 = 0x4,
+            byte4 = 0x8,
+            byte5 = 0x10,
+            byte6 = 0x20,
+            byte7 = 0x40,
+            byte8 = 0x80,
+        }
+
+        public enum UpdateTypes
+        {
+            UPDATETYPE_VALUES               = 0,
+            UPDATETYPE_MOVEMENT             = 1,
+            UPDATETYPE_CREATE_OBJECT        = 2,
+            UPDATETYPE_CREATE_OBJECT2       = 3,
+            UPDATETYPE_OUT_OF_RANGE_OBJECTS = 4,
+            UPDATETYPE_NEAR_OBJECTS         = 5,
+        }
+
+        [Flags]
+        public enum UpdateFlags
+        {
+            UPDATEFLAG_SELFTARGET   = 0x01,
+            UPDATEFLAG_TRANSPORT    = 0x02,
+            UPDATEFLAG_FULLGUID     = 0x04,
+            UPDATEFLAG_HIGHGUID     = 0x08,
+            UPDATEFLAG_ALL          = 0x10,
+            UPDATEFLAG_LIVING       = 0x20,
+            UPDATEFLAG_HASPOSITION  = 0x40
+        }
+
+        public enum ObjectTypes
+        {
+            TYPEID_OBJECT           = 0,
+            TYPEID_ITEM             = 1,
+            TYPEID_CONTAINER        = 2,
+            TYPEID_UNIT             = 3,
+            TYPEID_PLAYER           = 4,
+            TYPEID_GAMEOBJECT       = 5,
+            TYPEID_DYNAMICOBJECT    = 6,
+            TYPEID_CORPSE           = 7,
+            TYPEID_AIGROUP          = 8,
+            TYPEID_AREATRIGGER      = 9,
+        }
+    }
+}
