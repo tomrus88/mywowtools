4a38f4d2eb0705223e70e730c68d2fc33d8526e8
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2007-06-26 08:39:27 +0000

    Added Sha1 hash generator for mangos.

---
+++ trunk/pass_gen/pass_gen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace pass_gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider();
+
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine().ToUpper();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine().ToUpper();
+
+            string temp = username + ":" + password;
+
+            byte[] temp2 = new byte[temp.Length];
+
+            for(int i = 0; i < temp.Length;i++)
+            {
+                temp2[i] = (byte)temp[i];
+            }
+
+            sha.ComputeHash(temp2, 0, temp2.Length);
+
+            string hash = String.Empty;
+
+            for (int i = 0; i < sha.Hash.Length; i++)
+            {
+                hash += sha.Hash[i].ToString("X2");
+            }
+
+            Console.WriteLine("Hash: {0}", hash);
+            Console.ReadKey();
+        }
+    }
+}
