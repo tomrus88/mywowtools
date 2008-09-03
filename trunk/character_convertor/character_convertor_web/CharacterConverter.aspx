ebb6e74815a8c9f88b457ce9b8deb2865bcdce4b
hazzik@gmail.com <hazzik@gmail.com@65da6843-c32c-0410-bcab-a998ee0685c5>
2008-09-03 09:37:59 +0000

    Для извращенцев написал вебморду

---
+++ trunk/character_convertor/character_convertor_web/CharacterConverter.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="CharacterConverter.aspx.cs" Inherits="CharacterConverterView" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Character converter</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table >
+    <tr>
+    <td>Db host:</td>
+    <td><asp:TextBox ID="TextBox1" runat="server">localhost</asp:TextBox></td>
+    </tr>
+    <tr>
+    <td>Db port:</td>
+    <td><asp:TextBox ID="TextBox2" runat="server">3306</asp:TextBox></td>
+    </tr>
+    <tr>
+    <td>Db base:</td>
+    <td><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></td>
+    </tr>
+    <tr>
+    <td>Db user:</td>
+    <td><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></td>
+    </tr>
+    <tr>
+    <td>Db pass:</td>
+    <td><asp:TextBox ID="TextBox5" runat="server"></asp:TextBox></td>
+    </tr>
+    <td colspan = "2" >
+		<asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Button" />
+    </td>
+    <tr>
+    </tr>
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/character_convertor/character_convertor_web/CharacterConverter.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using CharacterConverter;
+
+public partial class CharacterConverterView : System.Web.UI.Page, IConverterView {
+	private ConverterPresenter _presenter;
+	public CharacterConverterView() {
+		_presenter = new ConverterPresenter(this);
+	}
+	protected void Page_Load(object sender, EventArgs e) {
+
+	}
+
+	public string Host {
+		get { return this.TextBox1.Text; }
+		set { this.TextBox1.Text = value; }
+	}
+
+	public string Port {
+		get { return this.TextBox2.Text; }
+		set { this.TextBox2.Text = value; }
+	}
+
+	public string Base {
+		get { return this.TextBox3.Text; }
+		set { this.TextBox3.Text = value; }
+	}
+
+	public new string User {
+		get { return this.TextBox4.Text; }
+		set { this.TextBox4.Text = value; }
+	}
+
+	public string Pass {
+		get { return this.TextBox5.Text; }
+		set { this.TextBox5.Text = value; }
+	}
+
+	public void SetPresenter(ConverterPresenter presenter) {
+		_presenter = presenter;
+	}
+
+	protected void Button1_Click(object sender, EventArgs e) {
+		_presenter.Convert();
+	}
+}
