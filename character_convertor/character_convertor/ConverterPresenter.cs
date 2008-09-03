4f388899817e529cd952e2fab55937067ada3d57
hazzik@gmail.com <hazzik@gmail.com@65da6843-c32c-0410-bcab-a998ee0685c5>
2008-09-03 09:06:47 +0000

    Учимся писать программы "правильно". Создание различных пользовательских интерфейсов реализовано посредствам паттерна MVP

---
+++ character_convertor/character_convertor/ConverterPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharacterConverter {
+	public class ConverterPresenter {
+		private Converter _converter;
+		private IConverterView _view;
+		public ConverterPresenter(IConverterView view) {
+			_view = view;
+			_view.SetPresenter(this);
+		}
+
+		public void Convert() {
+			var cs = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4}",
+				_view.Host, _view.Port, _view.Base, _view.User, _view.Pass);
+
+			_converter = new Converter(cs);
+			_converter.Convert();
+		}
+	}
+}
---
+++ character_convertor/character_convertor_console/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharacterConverter.Con {
+	class Program : IConverterView {
+		private ConverterPresenter _presenter;
+
+		public Program() {
+			_presenter = new ConverterPresenter(this);
+		}
+
+		private string _host;
+		public string Host {
+			get {
+				if(_host == null) {
+					Console.Write("Enter DB host: ");
+					_host = Console.ReadLine();
+				}
+				return _host;
+			}
+			set { _host = value; }
+		}
+
+		private string _port;
+		public string Port {
+			get {
+				if(_port == null) {
+					Console.Write("Enter DB port: ");
+					_port = Console.ReadLine();
+				}
+				return _port;
+			}
+			set { _port = value; }
+		}
+
+		private string _base;
+		public string Base {
+			get {
+				if(_base == null) {
+					Console.Write("Enter DB name: ");
+					_base = Console.ReadLine();
+				}
+				return _base;
+			}
+			set { _base = value; }
+		}
+
+		private string _user;
+		public string User {
+			get {
+				if(_user == null) {
+					Console.Write("Enter DB user name: ");
+					_user = Console.ReadLine();
+				}
+				return _user;
+			}
+			set { _user = value; }
+		}
+
+		private string _pass;
+		public string Pass {
+			get {
+				if(_pass == null) {
+					Console.Write("Enter DB password: ");
+					_pass = Console.ReadLine();
+				}
+				return _pass;
+			}
+			set { _pass = value; }
+		}
+
+		public void SetPresenter(ConverterPresenter presenter) {
+			_presenter = presenter;
+		}
+
+		private void Convert() {
+			_presenter.Convert();
+		}
+
+		static void Main(string[] args) {
+			Console.WriteLine("Characters convertor for MaNGoS");
+			Console.WriteLine("Client version 2.4.2->2.4.3");
+			Console.WriteLine("Written by TOM_RUS");
+
+			AppDomain currentDomain = AppDomain.CurrentDomain;
+			currentDomain.UnhandledException += new UnhandledExceptionEventHandler(TopLevelErrorHandler);
+
+			Program program = new Program();
+			program.Convert();
+
+			Console.WriteLine("Done!");
+			Console.ReadKey();
+		}
+
+		private static void TopLevelErrorHandler(object sender, UnhandledExceptionEventArgs args) {
+			Exception e = (Exception)args.ExceptionObject;
+			Console.WriteLine("Error Occured : " + e.ToString());
+		}
+	}
+}
