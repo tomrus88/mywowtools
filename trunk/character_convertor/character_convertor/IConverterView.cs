af565f863ad1c7ad3824e6245a0b0c8f34b7aa42
hazzik@gmail.com <hazzik@gmail.com@65da6843-c32c-0410-bcab-a998ee0685c5>
2008-09-03 09:06:47 +0000

    Учимся писать программы "правильно". Создание различных пользовательских интерфейсов реализовано посредствам паттерна MVP

---
+++ trunk/character_convertor/character_convertor/IConverterView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharacterConverter {
+	public interface IConverterView {
+		string Host { get; set; }
+		string Port { get; set; }
+		string Base { get; set; }
+		string User { get; set; }
+		string Pass { get; set; }
+
+		void SetPresenter(ConverterPresenter presenter);
+	}
+}
---
+++ trunk/character_convertor/character_convertor_gui/FrmMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CharacterConverter.Gui {
+	public partial class FrmMain : Form, IConverterView {
+		private ConverterPresenter _presenter;
+		public FrmMain() {
+			InitializeComponent();
+
+			_presenter = new ConverterPresenter(this);
+		}
+
+		public string Host {
+			get { return textBox1.Text; }
+			set { textBox1.Text = value; }
+		}
+
+		public string Port {
+			get { return textBox2.Text; }
+			set { textBox2.Text = value; }
+		}
+
+		public string Base {
+			get { return textBox3.Text; }
+			set { textBox3.Text = value; }
+		}
+
+		public string User {
+			get { return textBox4.Text; }
+			set { textBox4.Text = value; }
+		}
+
+		public string Pass {
+			get { return textBox5.Text; }
+			set { textBox5.Text = value; }
+		}
+
+		public void SetPresenter(ConverterPresenter presenter) {
+			_presenter = presenter;
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			_presenter.Convert();
+		}
+	}
+}
