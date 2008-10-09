498229ff22e1e71767d82bbf521343b1994b002f
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2008-10-09 15:30:18 +0000

    - Добавлен поиск и сохранение в текстовый формат.

--- WoWPacketViewer/WoWPacketViewer/Form1.cs
+++ WoWPacketViewer/WoWPacketViewer/Form1.cs
@@ -7,6 +7,7 @@ namespace WoWPacketViewer
     public partial class Form1 : Form
     {
         PacketViewerBase m_packetViewer;
+        SearchForm m_searchForm;
 
         public Form1()
         {
@@ -28,18 +29,19 @@ namespace WoWPacketViewer
                 listView1.Items.Clear();
 
                 toolStripStatusLabel1.Text = "Loading...";
-					var file = openFileDialog1.FileName;
-					switch(Path.GetExtension(file)) {
-					case ".bin":
-						m_packetViewer = new WowCorePacketViewer();
-						break;
-					case ".sqlite":
-						m_packetViewer = new SqLitePacketViewer();
-						break;
-					default:
-						break;
-					}
-					 m_packetViewer.LoadData(openFileDialog1.FileName);
+                var file = openFileDialog1.FileName;
+                switch (Path.GetExtension(file))
+                {
+                    case ".bin":
+                        m_packetViewer = new WowCorePacketViewer();
+                        break;
+                    case ".sqlite":
+                        m_packetViewer = new SqLitePacketViewer();
+                        break;
+                    default:
+                        break;
+                }
+                m_packetViewer.LoadData(openFileDialog1.FileName);
                 toolStripStatusLabel1.Text = openFileDialog1.FileName;
                 listView1.Items.AddRange(m_packetViewer.ListPackets());
             }
@@ -55,6 +57,83 @@ namespace WoWPacketViewer
                 return;
             m_packetViewer.ShowHex(textBox1, sic[0]);
             m_packetViewer.ShowParsed(textBox2, sic[0]);
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (m_searchForm == null || m_searchForm.IsDisposed)
+                m_searchForm = new SearchForm();
+
+            if (m_searchForm.Visible)
+                return;
+
+            m_searchForm.Show();
+        }
+
+        public void Search(string opcode, bool direction)
+        {
+            if (String.IsNullOrEmpty(opcode))
+                return;
+
+            int selectedIndex = 0;
+            ListView.SelectedIndexCollection sic = listView1.SelectedIndices;
+            if (sic.Count != 0)
+                selectedIndex = sic[0];
+
+            if (!direction)
+            {
+                ListViewItem i = listView1.FindItemWithText(opcode, true, selectedIndex + 1);
+                if (i != null)
+                {
+                    i.Selected = true;
+                    i.EnsureVisible();
+                }
+            }
+            else
+            {
+                // не пашет :(
+                for (int i = listView1.Items.Count - 1; i != 0; --i)
+                {
+                    ListViewItem j = listView1.Items[i];
+                    if (j.Text.Contains(opcode))
+                    {
+                        j.Selected = true;
+                        j.EnsureVisible();
+                        break;
+                    }
+
+                    System.Collections.IEnumerator gg = j.SubItems.GetEnumerator();
+                    bool exit = false;
+                    while (gg.MoveNext())
+                    {
+                        ListViewItem.ListViewSubItem k = (ListViewItem.ListViewSubItem)gg.Current;
+                        if(k.Text.Contains(opcode))
+                        {
+                            j.Selected = true;
+                            j.EnsureVisible();
+                            exit = true;
+                            break;
+                        }
+                    }
+
+                    if (exit)
+                        break;
+                }
+            }
+        }
+
+        private void saveAsTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter stream = new StreamWriter(saveFileDialog1.OpenFile());
+                foreach (Packet p in PacketViewerBase.Packets)
+                {
+                    stream.Write(m_packetViewer.HexLike(p));
+                }
+                stream.Flush();
+                stream.Close();
+            }
         }
     }
 }
