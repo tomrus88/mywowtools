ead57d814af566fb16fff40c58f542c801350c3c
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2008-10-09 15:30:18 +0000

    - Добавлен поиск и сохранение в текстовый формат.

--- trunk/WoWPacketViewer/WoWPacketViewer/PacketViewer.cs
+++ trunk/WoWPacketViewer/WoWPacketViewer/PacketViewer.cs
@@ -5,88 +5,108 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace WoWPacketViewer {
-	public abstract class PacketViewerBase {
-		//TODO: сделать по аналогии с update парсером
-		protected List<Packet> m_packets = new List<Packet>();
+namespace WoWPacketViewer
+{
+    public abstract class PacketViewerBase
+    {
+        //TODO: сделать по аналогии с update парсером
+        protected static List<Packet> m_packets = new List<Packet>();
 
-		public abstract int LoadData(string file);
+        public static List<Packet> Packets
+        {
+            get { return m_packets; }
+        }
 
-		public ListViewItem[] ListPackets() {
-			var pkt = (from packet in m_packets
-						  where packet.Opcode == OpCodes.CMSG_AUTH_SESSION
-						  select packet).FirstOrDefault();
+        public abstract int LoadData(string file);
 
-			uint build;
-			try {
-				build = BitConverter.ToUInt32(pkt.Data, 0);
-			}
-			catch(Exception exc) {
-				MessageBox.Show(exc.Message);
-				return null;
-			}
+        public ListViewItem[] ListPackets()
+        {
+            var pkt = (from packet in m_packets
+                       where packet.Opcode == OpCodes.CMSG_AUTH_SESSION
+                       select packet).FirstOrDefault();
 
-			var items =new List<ListViewItem>();
-			foreach(Packet p in m_packets) {
-				ListViewItem item;
-				if(p.Direction == Direction.CLIENT)
-					item = new ListViewItem(new string[] { build.ToString(), p.Opcode.ToString(), string.Empty, p.Data.Length.ToString() });
-				else
-					item = new ListViewItem(new string[] { build.ToString(), string.Empty, p.Opcode.ToString(), p.Data.Length.ToString() });
-				items.Add(item);
-			}
-			return items.ToArray();
-		}
+            uint build;
+            try
+            {
+                build = BitConverter.ToUInt32(pkt.Data, 0);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                return null;
+            }
 
-		public void ShowHex(TextBox tb, int index) {
-			Packet pkt = m_packets[index];
+            var items = new List<ListViewItem>();
+            foreach (Packet p in m_packets)
+            {
+                ListViewItem item;
+                if (p.Direction == Direction.CLIENT)
+                    item = new ListViewItem(new string[] { build.ToString(), p.Opcode.ToString(), string.Empty, p.Data.Length.ToString() });
+                else
+                    item = new ListViewItem(new string[] { build.ToString(), string.Empty, p.Opcode.ToString(), p.Data.Length.ToString() });
+                items.Add(item);
+            }
+            return items.ToArray();
+        }
 
-			tb.Text = HexLike(pkt);
-		}
+        public void ShowHex(TextBox tb, int index)
+        {
+            Packet pkt = m_packets[index];
 
-		public string HexLike(Packet pkt) {
+            tb.Text = HexLike(pkt);
+        }
 
-			var length = pkt.Data.Length;
-			var dir = (pkt.Direction == Direction.CLIENT) ? "C->S" : "S->C";
+        public string HexLike(Packet pkt)
+        {
 
-			var result = new StringBuilder();
-			result.AppendFormat("Packet {0}, {1} ({2}), len {3}", dir, pkt.Opcode, (ushort)pkt.Opcode, length);
-			result.AppendLine();
+            var length = pkt.Data.Length;
+            var dir = (pkt.Direction == Direction.CLIENT) ? "C->S" : "S->C";
 
-			if(length == 0) { // empty packet (no data provided)
-				result.AppendLine("0000: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- | ................");
-			}
-			else {
-				int offset = 0;
-				for(int i = 0; i < length; i += 0x10) {
-					var bytes = new StringBuilder();
-					var chars = new StringBuilder();
+            var result = new StringBuilder();
+            result.AppendFormat("Packet {0}, {1} ({2}), len {3}", dir, pkt.Opcode, (ushort)pkt.Opcode, length);
+            result.AppendLine();
 
-					for(int j = 0; j < 0x10; ++j) {
-						if(offset < length) {
-							int c = pkt.Data[offset];
-							offset++;
+            if (length == 0)
+            { // empty packet (no data provided)
+                result.AppendLine("0000: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- | ................");
+            }
+            else
+            {
+                int offset = 0;
+                for (int i = 0; i < length; i += 0x10)
+                {
+                    var bytes = new StringBuilder();
+                    var chars = new StringBuilder();
 
-							bytes.AppendFormat("{0,-3:X2}", c);
-							chars.Append((c >= 0x20 && c < 0x80) ? (char)c : '.');
-						}
-						else {
-							bytes.Append("-- ");
-							chars.Append('.');
-						}
-					}
+                    for (int j = 0; j < 0x10; ++j)
+                    {
+                        if (offset < length)
+                        {
+                            int c = pkt.Data[offset];
+                            offset++;
 
-					result.AppendLine(i.ToString("X4") + ": " + bytes.ToString() + "| " + chars.ToString());
-				}
-			}
+                            bytes.AppendFormat("{0,-3:X2}", c);
+                            chars.Append((c >= 0x20 && c < 0x80) ? (char)c : '.');
+                        }
+                        else
+                        {
+                            bytes.Append("-- ");
+                            chars.Append('.');
+                        }
+                    }
 
-			result.AppendLine();
+                    result.AppendLine(i.ToString("X4") + ": " + bytes.ToString() + "| " + chars.ToString());
+                }
+            }
 
-			return result.ToString();
-		}
+            result.AppendLine();
 
-		public void ShowParsed(TextBox tb, int index) {
+            return result.ToString();
+        }
 
-		}
-	}
+        public void ShowParsed(TextBox tb, int index)
+        {
+
+        }
+    }
 }
---
+++ trunk/WoWPacketViewer/WoWPacketViewer/SearchForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WoWPacketViewer
+{
+    public partial class SearchForm : Form
+    {
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ((Form1)Owner).Search(textBox1.Text, radioButton1.Checked ? true : false);
+        }
+    }
+}
