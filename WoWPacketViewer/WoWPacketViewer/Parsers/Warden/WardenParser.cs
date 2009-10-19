d97c6cbf10e6243349bfe691f869200db4b87ea0
hazzik <hazzik@65da6843-c32c-0410-bcab-a998ee0685c5>
2009-10-19 13:04:04 +0000

    WardenParser.cs - extracted methods to parse SMSG_ and CMSG_

--- WoWPacketViewer/WoWPacketViewer/Parsers/Warden/WardenParser.cs
+++ WoWPacketViewer/WoWPacketViewer/Parsers/Warden/WardenParser.cs
@@ -3,36 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Windows.Forms;
+using WoWPacketViewer.Parsers.Warden;
 
 namespace WoWPacketViewer.Parsers
 {
-    enum CheckType
-    {
-        MEM_CHECK       = 0,    // byte strIndex + uint Offset + byte Len (checks to ensure memory isn't modified?)
-        PAGE_CHECK_A    = 1,    // uint Seed + byte[20] SHA1 + uint Addr + byte Len
-        PAGE_CHECK_B    = 2,    // uint Seed + byte[20] SHA1 + uint Addr + byte Len
-        MPQ_CHECK       = 3,    // byte strIndex (checks to ensure MPQ file isn't modified?)
-        LUA_STR_CHECK   = 4,    // byte strIndex (checks to ensure LUA string isn't used?)
-        DRIVER_CHECK    = 5,    // uint Seed + byte[20] SHA1 + byte strIndex (checks to ensure driver isn't loaded?)
-        TIMING_CHECK    = 6,    // empty (checks to ensure TickCount isn't detoured?)
-        PROC_CHECK      = 7,    // uint Seed + byte[20] SHA1 + byte strIndex1 + byte strIndex2 + uint Offset + byte Len (checks to ensure proc isn't detoured?)
-        MODULE_CHECK    = 8,    // uint Seed + byte[20] SHA1 (checks to ensure module loaded?)
-    }
-
-    struct CheckInfo
-    {
-        public CheckType m_type;
-        public int m_length;
-
-        public CheckInfo(CheckType type, int len)
-        {
-            m_type = type;
-            m_length = len; // for MEM_CHECK result
-        }
-    }
-
-    [Parser(OpCodes.CMSG_WARDEN_DATA)]
+	[Parser(OpCodes.CMSG_WARDEN_DATA)]
     [Parser(OpCodes.SMSG_WARDEN_DATA)]
     internal class MSG_WARDEN_DATA : Parser
     {
@@ -51,131 +26,142 @@ namespace WoWPacketViewer.Parsers
             s_checkTypes = checkTypes;
         }
 
-        public override string Parse()
-        {
-            var gr = Packet.CreateReader();
+		public override string Parse()
+		{
+			switch (Packet.Code)
+			{
+				case OpCodes.CMSG_WARDEN_DATA:
+					{
+						return ParseCMSG_WARDEN_DATA();
+					}
 
-            switch (Packet.Code)
-            {
-                #region CMSG_WARDEN_DATA
-                case OpCodes.CMSG_WARDEN_DATA:
-                    {
-                        var wardenOpcode = gr.ReadByte();
-                        AppendFormatLine("C->S Warden Opcode: {0:X2}", wardenOpcode);
+				case OpCodes.SMSG_WARDEN_DATA:
+					{
+						return ParseSMSG_WARDEN_DATA();
+					}
+				default:
+					return String.Empty;
+			}
+		}
 
-                        switch (wardenOpcode)
-                        {
-                            case 0x00:
-                                {
-                                    AppendFormatLine("Load module failed or module is missing...");
-                                    AppendLine();
-                                }
-                                break;
-                            case 0x01:
-                                {
-                                    AppendFormatLine("Module loaded successfully...");
-                                    AppendLine();
-                                }
-                                break;
-                            case 0x02:
-                                Parse_CHEAT_CHECKS_RESULTS(gr);
-                                break;
-                            case 0x04:
-                                {
-                                    var hash = gr.ReadBytes(20); // SHA1 hash
-                                    AppendFormatLine("SHA1: 0x{0}", Utility.ByteArrayToHexString(hash));
-                                    AppendLine();
-                                }
-                                break;
-                            default:
-                                {
-                                    AppendFormatLine("Unknown warden opcode {0}", wardenOpcode);
-                                    AppendLine();
-                                }
-                                break;
-                        }
+		private string ParseSMSG_WARDEN_DATA()
+		{
+			BinaryReader gr;
+			gr = Packet.CreateReader();
 
-                        CheckPacket(gr);
+			s_lastCheckInfo.Clear();
 
-                        return GetParsedString();
-                    }
-                #endregion
-                #region SMSG_WARDEN_DATA
-                case OpCodes.SMSG_WARDEN_DATA:
-                    {
-                        s_lastCheckInfo.Clear();
+			var wardenOpcode = gr.ReadByte();
+			AppendFormatLine("S->C Warden Opcode: {0:X2}", wardenOpcode);
 
-                        var wardenOpcode = gr.ReadByte();
-                        AppendFormatLine("S->C Warden Opcode: {0:X2}", wardenOpcode);
+			switch (wardenOpcode)
+			{
+				case 0x00:
+					{
+						var md5 = gr.ReadBytes(16); // md5
+						var rc4 = gr.ReadBytes(16); // rc4 key
+						var len = gr.ReadInt32();   // len
+						AppendFormatLine("MD5: 0x{0}", Utility.ByteArrayToHexString(md5));
+						AppendFormatLine("RC4: 0x{0}", Utility.ByteArrayToHexString(rc4));
+						AppendFormatLine("Len: {0}", len);
+						AppendLine();
+					}
+					break;
+				case 0x01:
+					{
+						var len = gr.ReadInt16();
+						var chunk = gr.ReadBytes(len);
+						AppendFormatLine("Received warden module chunk, len {0}", len);
+						AppendLine();
+					}
+					break;
+				case 0x02:
+					Parse_CHEAT_CHECKS(gr);
+					break;
+				case 0x03:
+					{
+						while (gr.BaseStream.Position != gr.BaseStream.Length)
+						{
+							var len = gr.ReadInt16();
+							var checkSum = gr.ReadUInt32();
+							var data = gr.ReadBytes(len);
 
-                        switch (wardenOpcode)
-                        {
-                            case 0x00:
-                                {
-                                    var md5 = gr.ReadBytes(16); // md5
-                                    var rc4 = gr.ReadBytes(16); // rc4 key
-                                    var len = gr.ReadInt32();   // len
-                                    AppendFormatLine("MD5: 0x{0}", Utility.ByteArrayToHexString(md5));
-                                    AppendFormatLine("RC4: 0x{0}", Utility.ByteArrayToHexString(rc4));
-                                    AppendFormatLine("Len: {0}", len);
-                                    AppendLine();
-                                }
-                                break;
-                            case 0x01:
-                                {
-                                    var len = gr.ReadInt16();
-                                    var chunk = gr.ReadBytes(len);
-                                    AppendFormatLine("Received warden module chunk, len {0}", len);
-                                    AppendLine();
-                                }
-                                break;
-                            case 0x02:
-                                Parse_CHEAT_CHECKS(gr);
-                                break;
-                            case 0x03:
-                                {
-                                    while (gr.BaseStream.Position != gr.BaseStream.Length)
-                                    {
-                                        var len = gr.ReadInt16();
-                                        var checkSum = gr.ReadUInt32();
-                                        var data = gr.ReadBytes(len);
+							ValidateCheckSum(checkSum, data);
 
-                                        ValidateCheckSum(checkSum, data);
+							AppendFormatLine("Len: {0}", len);
+							AppendFormatLine("Checksum: 0x{0:X8}", checkSum);
+							AppendFormatLine("Data: 0x{0}", Utility.ByteArrayToHexString(data));
+							AppendLine();
+							if (gr.BaseStream.Position != gr.BaseStream.Length)
+								gr.ReadByte(); // next 0x03 opcode
+						}
+					}
+					break;
+				case 0x05:
+					{
+						var seed = gr.ReadBytes(16);
+						AppendFormatLine("Seed: 0x{0}", Utility.ByteArrayToHexString(seed));
+						AppendLine();
+					}
+					break;
+				default:
+					{
+						AppendFormatLine("Unknown warden opcode {0}", wardenOpcode);
+					}
+					break;
+			}
 
-                                        AppendFormatLine("Len: {0}", len);
-                                        AppendFormatLine("Checksum: 0x{0:X8}", checkSum);
-                                        AppendFormatLine("Data: 0x{0}", Utility.ByteArrayToHexString(data));
-                                        AppendLine();
-                                        if (gr.BaseStream.Position != gr.BaseStream.Length)
-                                            gr.ReadByte(); // next 0x03 opcode
-                                    }
-                                }
-                                break;
-                            case 0x05:
-                                {
-                                    var seed = gr.ReadBytes(16);
-                                    AppendFormatLine("Seed: 0x{0}", Utility.ByteArrayToHexString(seed));
-                                    AppendLine();
-                                }
-                                break;
-                            default:
-                                {
-                                    AppendFormatLine("Unknown warden opcode {0}", wardenOpcode);
-                                }
-                                break;
-                        }
+			CheckPacket(gr);
 
-                        CheckPacket(gr);
+			return GetParsedString();
+		}
 
-                        return GetParsedString();
-                    }
-                #endregion
-                default:
-                    return String.Empty;
-            }
-        }
+		private string ParseCMSG_WARDEN_DATA()
+		{
+			BinaryReader gr;
+			gr = Packet.CreateReader();
 
-        private void Parse_CHEAT_CHECKS_RESULTS(BinaryReader gr)
+			var wardenOpcode = gr.ReadByte();
+			AppendFormatLine("C->S Warden Opcode: {0:X2}", wardenOpcode);
+
+			switch (wardenOpcode)
+			{
+				case 0x00:
+					{
+						AppendFormatLine("Load module failed or module is missing...");
+						AppendLine();
+					}
+					break;
+				case 0x01:
+					{
+						AppendFormatLine("Module loaded successfully...");
+						AppendLine();
+					}
+					break;
+				case 0x02:
+					Parse_CHEAT_CHECKS_RESULTS(gr);
+					break;
+				case 0x04:
+					{
+						var hash = gr.ReadBytes(20); // SHA1 hash
+						AppendFormatLine("SHA1: 0x{0}", Utility.ByteArrayToHexString(hash));
+						AppendLine();
+					}
+					break;
+				default:
+					{
+						AppendFormatLine("Unknown warden opcode {0}", wardenOpcode);
+						AppendLine();
+					}
+					break;
+			}
+
+			CheckPacket(gr);
+
+			return GetParsedString();
+		}
+
+		private void Parse_CHEAT_CHECKS_RESULTS(BinaryReader gr)
         {
             var bufLen = gr.ReadUInt16();
             var checkSum = gr.ReadUInt32();
