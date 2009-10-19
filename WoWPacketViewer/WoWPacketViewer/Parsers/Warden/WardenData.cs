886d0b0093c688357e777f2c66c24612de73a526
hazzik <hazzik@65da6843-c32c-0410-bcab-a998ee0685c5>
2009-10-19 13:45:34 +0000

    ShowForm moved to WardenData class

--- WoWPacketViewer/WoWPacketViewer/Parsers/Warden/WardenData.cs
+++ WoWPacketViewer/WoWPacketViewer/Parsers/Warden/WardenData.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace WoWPacketViewer.Parsers.Warden
 {
@@ -6,11 +8,60 @@ namespace WoWPacketViewer.Parsers.Warden
 	{
 		public static IList<CheckInfo> CheckInfos = new List<CheckInfo>();
 		public static IDictionary<byte, CheckType> CheckTypes = new Dictionary<byte, CheckType>();
-		public static FrmWardenDebug WardenDebugForm;
+		private static FrmWardenDebug wardenDebugForm;
 
 		public static void InitCheckTypes(IDictionary<byte, CheckType> checkTypes)
 		{
 			CheckTypes = checkTypes;
+		}
+
+		public static void ShowForm(IEnumerable<string> strings, byte[] checks)
+		{
+			if (wardenDebugForm == null || wardenDebugForm.IsDisposed)
+			{
+				wardenDebugForm = new FrmWardenDebug();
+			}
+
+			TextBox[] textBoxes = wardenDebugForm.GetTextBoxes();
+			if (textBoxes.Length != 0)
+			{
+				foreach (TextBox tb in textBoxes)
+				{
+					if (tb.Name == "textBox1")
+					{
+						tb.Text = String.Empty;
+						foreach (string str in strings)
+						{
+							tb.Text += str;
+							tb.Text += "\r\n";
+						}
+						tb.Text += Utility.PrintHex(checks, 0, checks.Length);
+						continue;
+					}
+
+					byte val = 0;
+					if (GetByteForCheckType((CheckType) tb.TabIndex, ref val))
+						tb.Text = String.Format("{0:X2}", val);
+				}
+			}
+
+			if (!wardenDebugForm.Visible)
+			{
+				wardenDebugForm.Show();
+			}
+		}
+
+		private static bool GetByteForCheckType(CheckType checkType, ref byte val)
+		{
+			foreach (var temp in CheckTypes)
+			{
+				if (temp.Value == checkType)
+				{
+					val = temp.Key;
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 }
