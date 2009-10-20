974ef9ca1176607fc5204f3d57d4a42ff9ade8d8
hazzik <hazzik@65da6843-c32c-0410-bcab-a998ee0685c5>
2009-10-20 16:53:39 +0000

    WowCorePacketReader - code cleanup

--- WowTools/src/WoWPacketViewer/WowCorePacketReader.cs
+++ WowTools/src/WoWPacketViewer/WowCorePacketReader.cs
@@ -10,30 +10,30 @@ namespace WoWPacketViewer
 
 		public IEnumerable<Packet> ReadPackets(string file)
 		{
-			var packets = new List<Packet>();
-
-			var gr = new BinaryReader(new FileStream(file, FileMode.Open, FileAccess.Read), Encoding.ASCII);
-			gr.ReadBytes(3); // PKT
-			gr.ReadBytes(2); // 0x02, 0x02
-			gr.ReadByte(); // 0x06
-			Build = gr.ReadUInt16(); // build
-			gr.ReadBytes(4); // client locale
-			gr.ReadBytes(20); // packet key
-			gr.ReadBytes(64); // realm name
-
-			while (gr.PeekChar() >= 0)
+			using (var gr = new BinaryReader(new FileStream(file, FileMode.Open, FileAccess.Read), Encoding.ASCII))
 			{
-				Direction direction = gr.ReadByte() == 0xff ? Direction.Server : Direction.Client;
-				uint unixtime = gr.ReadUInt32();
-				uint tickcount = gr.ReadUInt32();
-				uint size = gr.ReadUInt32();
-				OpCodes opcode = (direction == Direction.Client) ? (OpCodes) gr.ReadUInt32() : (OpCodes) gr.ReadUInt16();
-				byte[] data = gr.ReadBytes((int) size - ((direction == Direction.Client) ? 4 : 2));
+				gr.ReadBytes(3); // PKT
+				gr.ReadBytes(2); // 0x02, 0x02
+				gr.ReadByte(); // 0x06
+				Build = gr.ReadUInt16(); // build
+				gr.ReadBytes(4); // client locale
+				gr.ReadBytes(20); // packet key
+				gr.ReadBytes(64); // realm name
 
-				packets.Add(new Packet(direction, opcode, data, unixtime, tickcount));
+				var packets = new List<Packet>();
+				while (gr.PeekChar() >= 0)
+				{
+					Direction direction = gr.ReadByte() == 0xff ? Direction.Server : Direction.Client;
+					uint unixtime = gr.ReadUInt32();
+					uint tickcount = gr.ReadUInt32();
+					uint size = gr.ReadUInt32();
+					OpCodes opcode = (direction == Direction.Client) ? (OpCodes) gr.ReadUInt32() : (OpCodes) gr.ReadUInt16();
+					byte[] data = gr.ReadBytes((int) size - ((direction == Direction.Client) ? 4 : 2));
+
+					packets.Add(new Packet(direction, opcode, data, unixtime, tickcount));
+				}
+				return packets;
 			}
-			gr.Close();
-			return packets;
 		}
 	}
 }
