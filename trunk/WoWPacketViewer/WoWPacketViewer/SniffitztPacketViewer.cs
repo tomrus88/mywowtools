4c721e6e14adfdf6f385f0263d66825a21030e77
hazzik <hazzik@65da6843-c32c-0410-bcab-a998ee0685c5>
2009-10-20 14:33:33 +0000

    refactoring

--- trunk/WoWPacketViewer/WoWPacketViewer/SniffitztPacketViewer.cs
+++ trunk/WoWPacketViewer/WoWPacketViewer/SniffitztPacketViewer.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
@@ -7,19 +9,20 @@ namespace WoWPacketViewer
 {
     public class SniffitztPacketViewer : PacketViewerBase
     {
-        public override int LoadData(string file)
-        {
-            var uri = new Uri(Path.GetFullPath(file)).ToString();
-            var doc = XDocument.Load(uri);
-            foreach (var packet in doc.XPathSelectElements("*/packet"))
-            {
-                var direction = (string)packet.Attribute("direction") == "S2C"
-                                          ? Direction.Server
-                                          : Direction.Client;
-                var opcode = (OpCodes)(uint)packet.Attribute("opcode");
-                m_packets.Add(new Packet(direction, opcode, Utility.HexStringToBinary(packet.Value), 0, 0));
-            }
-            return m_packets.Count;
-        }
+    	public override IEnumerable<Packet> ReadPackets(string file)
+    	{
+    		var packets = new List<Packet>();
+    		var uri = new Uri(Path.GetFullPath(file)).ToString();
+    		var doc = XDocument.Load(uri);
+    		foreach (var packet in doc.XPathSelectElements("*/packet"))
+    		{
+    			var direction = (string)packet.Attribute("direction") == "S2C"
+    			                	? Direction.Server
+    			                	: Direction.Client;
+    			var opcode = (OpCodes)(uint)packet.Attribute("opcode");
+    			packets.Add(new Packet(direction, opcode, Utility.HexStringToBinary(packet.Value), 0, 0));
+    		}
+    		return packets;
+    	}
     }
 }
