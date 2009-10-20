47c47912f4e02889aa824dfbdddc7e6f8029993c
hazzik <hazzik@65da6843-c32c-0410-bcab-a998ee0685c5>
2009-10-20 17:40:15 +0000

    Packet.HexLike moved to Utility

--- trunk/WowTools/src/WoWPacketViewer/Packet.cs
+++ trunk/WowTools/src/WoWPacketViewer/Packet.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using WowTools.Core;
 
 namespace WoWPacketViewer
@@ -29,53 +27,6 @@ namespace WoWPacketViewer
         public BinaryReader CreateReader()
         {
             return new BinaryReader(new MemoryStream(Data));
-        }
-
-        public string HexLike()
-        {
-            var length = Data.Length;
-            var dir = (Direction == Direction.Client) ? "C->S" : "S->C";
-
-            var result = new StringBuilder();
-            result.AppendFormat("Packet {0}, {1} ({2}), len {3}", dir, Code, (ushort)Code, length);
-            result.AppendLine();
-
-            if (length == 0)
-            {
-                result.AppendLine("0000: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- : ................");
-            }
-            else
-            {
-                var offset = 0;
-                for (var i = 0; i < length; i += 0x10)
-                {
-                    var bytes = new StringBuilder();
-                    var chars = new StringBuilder();
-
-                    for (var j = 0; j < 0x10; ++j)
-                    {
-                        if (offset < length)
-                        {
-                            int c = Data[offset];
-                            offset++;
-
-                            bytes.AppendFormat("{0,-3:X2}", c);
-                            chars.Append((c >= 0x20 && c < 0x80) ? (char)c : '.');
-                        }
-                        else
-                        {
-                            bytes.Append("-- ");
-                            chars.Append('.');
-                        }
-                    }
-
-                    result.AppendLine(i.ToString("X4") + ": " + bytes + ": " + chars);
-                }
-            }
-
-            result.AppendLine();
-
-            return result.ToString();
         }
     }
 }
