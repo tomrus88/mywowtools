12314c3913a9ab3aeeddb0fb56b0c3deb6b4af2a
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2009-10-25 13:42:24 +0000

    - More tabs to spaces replacements.

--- trunk/WowTools/src/UpdatePacketParser/SplineInfo.cs
+++ trunk/WowTools/src/UpdatePacketParser/SplineInfo.cs
@@ -4,70 +4,70 @@ using WowTools.Core;
 
 namespace WoWObjects
 {
-	public class SplineInfo
-	{
-		public SplineFlags Flags { get; private set; }
-		public Coords3 Point { get; private set; }
-		public ulong Guid { get; private set; }
-		public float Rotation { get; private set; }
-		public uint CurrentTime { get; private set; }
-		public uint FullTime { get; private set; }
-		public uint Unknown1 { get; private set; }
-		public float UnknownFloat1 { get; private set; }
-		public float UnknownFloat2 { get; private set; }
-		public float UnknownFloat3 { get; private set; }
-		public uint Unknown2 { get; private set; }
-		public uint Count { get; private set; }
-		private readonly List<Coords3> splines = new List<Coords3>();
-		public byte Unknown3 { get; private set; }
-		public Coords3 EndPoint { get; private set; }
+    public class SplineInfo
+    {
+        public SplineFlags Flags { get; private set; }
+        public Coords3 Point { get; private set; }
+        public ulong Guid { get; private set; }
+        public float Rotation { get; private set; }
+        public uint CurrentTime { get; private set; }
+        public uint FullTime { get; private set; }
+        public uint Unknown1 { get; private set; }
+        public float UnknownFloat1 { get; private set; }
+        public float UnknownFloat2 { get; private set; }
+        public float UnknownFloat3 { get; private set; }
+        public uint Unknown2 { get; private set; }
+        public uint Count { get; private set; }
+        private readonly List<Coords3> splines = new List<Coords3>();
+        public byte Unknown3 { get; private set; }
+        public Coords3 EndPoint { get; private set; }
 
-		public List<Coords3> Splines
-		{
-			get { return splines; }
-		}
+        public List<Coords3> Splines
+        {
+            get { return splines; }
+        }
 
-		public static SplineInfo Read(BinaryReader gr)
-		{
-			var spline = new SplineInfo();
-			spline.Flags = (SplineFlags) gr.ReadUInt32();
+        public static SplineInfo Read(BinaryReader gr)
+        {
+            var spline = new SplineInfo();
+            spline.Flags = (SplineFlags)gr.ReadUInt32();
 
-			if ((spline.Flags & SplineFlags.POINT) != 0)
-			{
-				spline.Point = gr.ReadCoords3();
-			}
+            if ((spline.Flags & SplineFlags.POINT) != 0)
+            {
+                spline.Point = gr.ReadCoords3();
+            }
 
-			if ((spline.Flags & SplineFlags.TARGET) != 0)
-			{
-				spline.Guid = gr.ReadUInt64();
-			}
+            if ((spline.Flags & SplineFlags.TARGET) != 0)
+            {
+                spline.Guid = gr.ReadUInt64();
+            }
 
-			if ((spline.Flags & SplineFlags.ORIENT) != 0)
-			{
-				spline.Rotation = gr.ReadSingle();
-			}
+            if ((spline.Flags & SplineFlags.ORIENT) != 0)
+            {
+                spline.Rotation = gr.ReadSingle();
+            }
 
-			spline.CurrentTime = gr.ReadUInt32();
-			spline.FullTime = gr.ReadUInt32();
-			spline.Unknown1 = gr.ReadUInt32();
+            spline.CurrentTime = gr.ReadUInt32();
+            spline.FullTime = gr.ReadUInt32();
+            spline.Unknown1 = gr.ReadUInt32();
 
-			spline.UnknownFloat1 = gr.ReadSingle();
-			spline.UnknownFloat2 = gr.ReadSingle();
-			spline.UnknownFloat3 = gr.ReadSingle();
+            spline.UnknownFloat1 = gr.ReadSingle();
+            spline.UnknownFloat2 = gr.ReadSingle();
+            spline.UnknownFloat3 = gr.ReadSingle();
 
-			spline.Unknown2 = gr.ReadUInt32();
+            spline.Unknown2 = gr.ReadUInt32();
 
-			spline.Count = gr.ReadUInt32();
+            spline.Count = gr.ReadUInt32();
 
-			for (uint i = 0; i < spline.Count; ++i)
-			{
-				spline.splines.Add(gr.ReadCoords3());
-			}
+            for (uint i = 0; i < spline.Count; ++i)
+            {
+                spline.splines.Add(gr.ReadCoords3());
+            }
 
-			spline.Unknown3 = gr.ReadByte(); // added in 3.0.8
+            spline.Unknown3 = gr.ReadByte(); // added in 3.0.8
 
-			spline.EndPoint = gr.ReadCoords3();
-			return spline;
-		}
-	};
+            spline.EndPoint = gr.ReadCoords3();
+            return spline;
+        }
+    };
 }
--- trunk/WowTools/src/UpdatePacketParser/TransportInfo.cs
+++ trunk/WowTools/src/UpdatePacketParser/TransportInfo.cs
@@ -3,22 +3,22 @@ using WowTools.Core;
 
 namespace WoWObjects
 {
-	public class TransportInfo
-	{
-		public ulong Guid { get; private set; }
-		public Coords4 Position { get; private set; }
-		public uint Time { get; private set; }
-		public byte Seat { get; private set; }
+    public class TransportInfo
+    {
+        public ulong Guid { get; private set; }
+        public Coords4 Position { get; private set; }
+        public uint Time { get; private set; }
+        public byte Seat { get; private set; }
 
-		public static TransportInfo Read(BinaryReader reader)
-		{
-			return new TransportInfo
-			       	{
-			       		Guid = reader.ReadPackedGuid(),
-			       		Position = reader.ReadCoords4(),
-			       		Time = reader.ReadUInt32(),
-			       		Seat = reader.ReadByte()
-			       	};
-		}
-	} ;
+        public static TransportInfo Read(BinaryReader reader)
+        {
+            return new TransportInfo
+            {
+                Guid = reader.ReadPackedGuid(),
+                Position = reader.ReadCoords4(),
+                Time = reader.ReadUInt32(),
+                Seat = reader.ReadByte()
+            };
+        }
+    }
 }
