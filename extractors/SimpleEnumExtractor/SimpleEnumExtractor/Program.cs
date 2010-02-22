6956620cb7238dad9e46ba28e0f6e524ad594dd1
hazzik <hazzik@65da6843-c32c-0410-bcab-a998ee0685c5>
2010-02-22 18:39:22 +0000

    small improvements

--- extractors/SimpleEnumExtractor/SimpleEnumExtractor/Program.cs
+++ extractors/SimpleEnumExtractor/SimpleEnumExtractor/Program.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Microsoft.Win32;
 using WoWReader;
 using System.CodeDom;
-using Microsoft.CSharp;
 using System.CodeDom.Compiler;
 
 namespace SimpleEnumExtractor {
 	class Program {
-		static GenericReader gr = null;
-		static TextReader tr = null;
+		static GenericReader gr;
+		static TextReader tr;
 
-		static string stream_string = String.Empty;
+		static string streamString = String.Empty;
 
 		static void Main(string[] args) {
-			// Ищем файл wow.exe сначала в текущей папке,
+		    // Ищем файл wow.exe сначала в текущей папке,
 			// затем лезем в реестр и смотрим папку там.
 			// TODO: взять путь к wow.exe из аргументов ком. строки
 			var wowPath = "WoW.exe";
@@ -26,43 +26,55 @@ namespace SimpleEnumExtractor {
 				wowPath = Path.Combine(wowPath ?? string.Empty, "WoW.exe");
 			}
 
-			var wow_exe = new FileInfo(wowPath);
-			if(!wow_exe.Exists) {
-				Console.WriteLine("File {0} not found!", wow_exe.Name);
+			var wowExe = new FileInfo(wowPath);
+			if(!wowExe.Exists) {
+				Console.WriteLine("File {0} not found!", wowExe.Name);
 			}
 			else {
-				Console.WriteLine("File {0} found in {1}", wow_exe.Name, wow_exe.DirectoryName);
+				Console.WriteLine("File {0} found in {1}", wowExe.Name, wowExe.DirectoryName);
 				Console.WriteLine("Extracting...");
-				var stream = wow_exe.Open(FileMode.Open, FileAccess.Read);
-				gr = new GenericReader(stream, Encoding.ASCII);
-				tr = new StreamReader(stream, Encoding.ASCII);
+			    using (var stream = wowExe.Open(FileMode.Open, FileAccess.Read))
+			    {
+			        gr = new GenericReader(stream, Encoding.ASCII);
+			        tr = new StreamReader(stream, Encoding.ASCII);
 
-				stream_string = tr.ReadToEnd();
+			        streamString = tr.ReadToEnd();
 
-				ExtractEnum("ChatMsg", "CHAT_MSG_BATTLEGROUND_LEADER", "CHAT_MSG_ADDON", "{0}", -1);
-				ExtractEnum("ResponseCodes", "CHAR_NAME_DECLENSION_DOESNT_MATCH_BASE_NAME",
-					"RESPONSE_SUCCESS");
-				ExtractEnum("ChatNotify", "VOICE_OFF", "YOU_CHANGED", "CHAT_{0}_NOTICE", 0);
-				//ExtractEnum("OpCodes", "NUM_MSG_TYPES", "MSG_NULL_ACTION");
-				ExtractEnum("SpellFailedReason", "SPELL_FAILED_UNKNOWN",
-                    "SPELL_FAILED_SUCCESS");
-				ExtractEnum("EnchantConditions", "ENCHANT_CONDITION_REQUIRES",
-					"ENCHANT_CONDITION_EQUAL_VALUE");
-				ExtractEnum("ItemModType", "ITEM_MOD_SPELL_POWER", "ITEM_MOD_MANA");
+                    ExtractEnum("ChatMsg", "CHAT_MSG_GUILD_ACHIEVEMENT", "CHAT_MSG_ADDON", s => ToCamelCase(s.Substring("CHAT_MSG_".Length)), -1);
+			        ExtractEnum("ResponseCodes", "CHAR_NAME_DECLENSION_DOESNT_MATCH_BASE_NAME",
+			                    "RESPONSE_SUCCESS");
+			        ExtractEnum("ChatNotify", "VOICE_OFF", "YOU_CHANGED", a => string.Format("CHAT_{0}_NOTICE", a), 0);
+			        //ExtractEnum("OpCodes", "NUM_MSG_TYPES", "MSG_NULL_ACTION");
+			        ExtractEnum("SpellFailedReason", "SPELL_FAILED_UNKNOWN",
+			                    "SPELL_FAILED_SUCCESS");
+			        ExtractEnum("EnchantConditions", "ENCHANT_CONDITION_REQUIRES",
+			                    "ENCHANT_CONDITION_EQUAL_VALUE");
+			        ExtractEnum("ItemModType", "ITEM_MOD_SPELL_POWER", "ITEM_MOD_MANA");
 
-				tr.Close();
-				gr.Close();
-				stream.Close();
-				Console.WriteLine("Done");
+			        tr.Close();
+			        gr.Close();
+			    }
+			    Console.WriteLine("Done");
 			}
 			Console.Write("Press any key to exit...");
 			Console.ReadKey();
 		}
 
-		static void ExtractEnum(string name, string start, string end, string format, int index) {
+        static string ToCamelCase(string value)
+        {
+            return string.Concat(value
+                                     .Split('_')
+                                     .Select(str => char.ToUpperInvariant(str[0]) + str.Substring(1).ToLowerInvariant())
+                                     .ToArray());
+        }
+
+	    static void ExtractEnum(string name, string start, string end, Func<string, string> convertor, int index) {
 			try {
 				Console.Write("  Extracting {0,-61}", name);
-				DumpToFile(name, GetNames(start, end, format), index);
+			    var values = ReadValues(start, end)
+                    .Select(convertor)
+                    .ToList();
+			    DumpToFile(name, values, index);
 				Console.ForegroundColor = ConsoleColor.Green;
 				Console.WriteLine("done");
 			}
@@ -76,41 +88,23 @@ namespace SimpleEnumExtractor {
 		}
 
 		static void ExtractEnum(string name, string start, string end) {
-			ExtractEnum(name, start, end, "{0}", 0);
+			ExtractEnum(name, start, end, a => a, 0);
 		}
 
-		static string[] GetNames(string start, string end, string format) {
+		static IEnumerable<string> ReadValues(string start, string end) {
 			gr.BaseStream.Position = FindStartPos(start);
 			var names = new List<string>();
-			var name = string.Empty;
+			string value;
 			do {
-				name = gr.ReadStringNull();
-				names.Add(string.Format(format, name));
+				value = gr.ReadStringNull();
+			    names.Add(value);
 				FindNextPos(gr);
-			} while(name != end);
+			} while(value != end);
 			names.Reverse();
 			return names.ToArray();
 		}
 
-		static string[] GetNames(string start, string end) {
-			return GetNames(start, end, "{0}");
-		}
-
-		static void DumpEnumToFileOld(string enumName, string[] names, int first) {
-			using(var writer = new StreamWriter(enumName + ".h")) {
-				writer.WriteLine("enum " + enumName);
-				writer.WriteLine("{");
-
-				foreach(string str in names) {
-					writer.WriteLine("    {0,-45} = 0x{1:X2},", str, first);
-					first++;
-				}
-
-				writer.WriteLine("};");
-			}
-		}
-
-		static void DumpToFile(string enumName, string[] names, int first) {
+	    static void DumpToFile(string enumName, IEnumerable<string> names, int first) {
 			// TODO: Необходимо задавать провайдер из параметров командной строки
 			// по умолчанию брать Cpp провайдер
 			var @enum = new CodeTypeDeclaration(enumName) {
@@ -125,8 +119,8 @@ namespace SimpleEnumExtractor {
 				first++;
 			}
 
-			var codeProvider = CodeDomProvider.CreateProvider("Cpp");
-			var options = new CodeGeneratorOptions() {
+			var codeProvider = CodeDomProvider.CreateProvider("CSharp");
+			var options = new CodeGeneratorOptions {
 				BlankLinesBetweenMembers = false,
                 BracingStyle = "C",
 			};
@@ -136,19 +130,14 @@ namespace SimpleEnumExtractor {
 			}
 		}
 
-		static void DumpToFile(string enumName, string[] names) {
-			DumpToFile(enumName, names, 0);
+	    static int FindStartPos(string name) {
+			return streamString.IndexOf(name + '\0');
 		}
 
-		static int FindStartPos(string name) {
-			return stream_string.IndexOf(name + '\0');
-		}
-
-		static int FindNextPos(GenericReader reader) {
+		static void FindNextPos(BinaryReader reader) {
 			while(reader.PeekChar() == 0x00) {
 				reader.BaseStream.Position++;
 			}
-			return (int)reader.BaseStream.Position;
 		}
 	}
 }
