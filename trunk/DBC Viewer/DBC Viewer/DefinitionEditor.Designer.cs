ccda663d00db9aaf16e68b5c15b2a9bc4ce0e2cc
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2010-09-19 02:14:18 +0000

    - DBC Viewer: definition editor (disabled).

---
+++ trunk/DBC Viewer/DBC Viewer/DefinitionEditor.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DBC_Viewer
+{
+    partial class DefinitionEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column1,
+            this.Column2,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(713, 303);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_DefaultValuesNeeded);
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Index";
+            this.Column3.Name = "Column3";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Type";
+            this.Column2.Items.AddRange(new object[] {
+            "long",
+            "ulong",
+            "int",
+            "uint",
+            "short",
+            "ushort",
+            "sbyte",
+            "byte",
+            "float",
+            "double",
+            "string"});
+            this.Column2.MaxDropDownItems = 11;
+            this.Column2.Name = "Column2";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Index";
+            this.Column4.Name = "Column4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DefinitionEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "DefinitionEditor";
+            this.Text = "DefinitionEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column4;
+    }
+}
---
+++ trunk/DBC Viewer/DBC Viewer/DefinitionEditor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DBC_Viewer
+{
+    public partial class DefinitionEditor : Form
+    {
+        public DefinitionEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
+            WriteXml("test");
+            Close();
+        }
+
+        private void WriteXml(string name)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("dbclayout.xml");
+
+            XmlNode oldnode = doc["DBFilesClient"][name];
+            XmlElement newnode = doc.CreateElement(name);
+
+            if (oldnode == null)
+                doc["DBFilesClient"].AppendChild(newnode);
+            else
+                doc["DBFilesClient"].ReplaceChild(newnode, oldnode);
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null && row.Cells[2].Value != null && row.Cells[3].Value != null)
+                {
+                    if ((string)row.Cells[1].Value == String.Empty)
+                        continue;
+
+                    if ((bool)row.Cells[3].Value)
+                    {
+                        XmlElement index = doc.CreateElement("index");
+                        XmlNode primary = index.AppendChild(doc.CreateElement("primary"));
+                        primary.InnerText = (string)row.Cells[1].Value;
+                        doc["DBFilesClient"][name].AppendChild(index);
+                    }
+
+                    XmlElement ele = doc.CreateElement("field");
+                    ele.SetAttributeNode("type", "").Value = (string)row.Cells[2].Value;
+                    ele.SetAttributeNode("name", "").Value = (string)row.Cells[1].Value;
+                    doc["DBFilesClient"][name].AppendChild(ele);
+                }
+            }
+            doc.Save("dbclayout_test.xml");
+        }
+
+        public void SetDefinitions(XmlElement def)
+        {
+            XmlNodeList fields = def.GetElementsByTagName("field");
+            XmlNodeList indexes = def.GetElementsByTagName("index");
+            foreach (XmlNode field in fields)
+            {
+                var row = dataGridView1.Rows.Add();
+                ((DataGridViewCell)dataGridView1[0, row]).Value = row;
+                ((DataGridViewCell)dataGridView1[1, row]).Value = field.Attributes["name"].Value;
+                ((DataGridViewComboBoxCell)dataGridView1[2, row]).Value = field.Attributes["type"].Value;
+                ((DataGridViewCheckBoxCell)dataGridView1[3, row]).Value = IsIndexColumn(field.Attributes["name"].Value, indexes);
+            }
+        }
+
+        private bool IsIndexColumn(string name, XmlNodeList indexes)
+        {
+            foreach (XmlNode index in indexes)
+                if (index["primary"].InnerText == name)
+                    return true;
+            return false;
+        }
+
+        private void dataGridView1_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells[0].Value = dataGridView1.Rows.Count;
+            e.Row.Cells[1].Value = "";
+            e.Row.Cells[2].Value = "int";
+            e.Row.Cells[3].Value = false;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex != 0 || e.RowIndex == -1)
+                return;
+
+            try
+            {
+                string val = Convert.ToString(dataGridView1[e.ColumnIndex, e.RowIndex].Value);
+                dataGridView1[e.ColumnIndex, e.RowIndex].Value = int.Parse(val);
+            }
+            catch
+            {
+                dataGridView1[e.ColumnIndex, e.RowIndex].Value = null;
+            }
+        }
+    }
+}
