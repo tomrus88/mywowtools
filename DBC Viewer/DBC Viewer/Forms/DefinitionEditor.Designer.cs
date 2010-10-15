f14dbfb5b9298549f383222cbe618da16568d262
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2010-10-15 05:49:16 +0000

    - DBC Viewer: added clone button to DefinitionEditor.

--- DBC Viewer/DBC Viewer/Forms/DefinitionEditor.Designer.cs
+++ DBC Viewer/DBC Viewer/Forms/DefinitionEditor.Designer.cs
@@ -37,13 +37,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 224);
+            this.button1.Location = new System.Drawing.Point(120, 224);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(48, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Done";
             this.button1.UseVisualStyleBackColor = true;
@@ -104,9 +105,9 @@
             "float",
             "double",
             "string"});
-            this.comboBox1.Location = new System.Drawing.Point(363, 224);
+            this.comboBox1.Location = new System.Drawing.Point(381, 224);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(103, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.Visible = false;
             this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
@@ -116,7 +117,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(12, 224);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(48, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "Add";
             this.button2.UseVisualStyleBackColor = true;
@@ -126,7 +127,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(213, 226);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
             this.textBox1.TabIndex = 5;
             this.textBox1.Text = "0";
             // 
@@ -139,11 +140,22 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Build:";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clone";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // DefinitionEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 263);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -173,5 +185,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
