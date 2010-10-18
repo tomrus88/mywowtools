9122709faf11179e1562d14ae44a43531eb679cb
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2010-10-18 01:44:30 +0000

    - DBC Viewer: added index column to DefinitionEditor.

--- DBC Viewer/DBC Viewer/Forms/DefinitionEditor.cs
+++ DBC Viewer/DBC Viewer/Forms/DefinitionEditor.cs
@@ -47,17 +47,17 @@ namespace DBCViewer
 
             foreach (ListViewItem item in listView1.Items)
             {
-                if (item.SubItems[2].Text == "True")
+                if (item.SubItems[3].Text == "True")
                 {
                     XmlElement index = doc.CreateElement("index");
                     XmlNode primary = index.AppendChild(doc.CreateElement("primary"));
-                    primary.InnerText = item.SubItems[0].Text;
+                    primary.InnerText = item.SubItems[1].Text;
                     newnode.AppendChild(index);
                 }
 
                 XmlElement ele = doc.CreateElement("field");
-                ele.SetAttributeNode("type", "").Value = item.SubItems[1].Text;
-                ele.SetAttributeNode("name", "").Value = item.SubItems[0].Text;
+                ele.SetAttributeNode("type", "").Value = item.SubItems[2].Text;
+                ele.SetAttributeNode("name", "").Value = item.SubItems[1].Text;
                 newnode.AppendChild(ele);
             }
 
@@ -83,13 +83,21 @@ namespace DBCViewer
             XmlNodeList fields = def.GetElementsByTagName("field");
             XmlNodeList indexes = def.GetElementsByTagName("index");
 
+            var i = 0;
             foreach (XmlNode field in fields)
             {
                 listView1.Items.Add(new ListViewItem(new string[] {
+                    i.ToString(),
                     field.Attributes["name"].Value,
                     field.Attributes["type"].Value,
                     IsIndexColumn(field.Attributes["name"].Value, indexes).ToString()}));
+                i++;
             }
+        }
+
+        private void DefinitionEditor_Load(object sender, EventArgs e)
+        {
+            InitDefinitions();
         }
 
         private static bool IsIndexColumn(string name, XmlNodeList indexes)
@@ -128,34 +136,22 @@ namespace DBCViewer
         {
             var item = listView1.GetItemAt(e.X, e.Y);
             if (item != null)
-            {
-                var subItem = item.GetSubItemAt(e.X, e.Y);
-                if (item.SubItems.IndexOf(subItem) != 0)
-                    item.Selected = true;
-            }
+                item.Selected = true;
 
             if (e.Button == MouseButtons.Right && listView1.SelectedItems.Count > 0)
                 listView1.DoDragDrop(listView1.SelectedItems[0], DragDropEffects.Move);
-            else if (e.Button == MouseButtons.Left && comboBox1.Visible)
-                comboBox1.Visible = false;
+            else if (e.Button == MouseButtons.Left)
+            {
+                if (comboBox1.Visible)
+                    comboBox1.Hide();
+                if (textBox2.Visible)
+                    textBox2.Hide();
+            }
         }
 
         private void listView1_DragOver(object sender, DragEventArgs e)
         {
             e.Effect = DragDropEffects.Move;
-        }
-
-        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
-        {
-            m_listViewSubItem.Text = comboBox1.Text;        // Set text of ListView item
-
-            comboBox1.Visible = false;                      // Hide the combobox
-        }
-
-        private void comboBox1_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape)                   // Check if user pressed "ESC"
-                comboBox1.Visible = false;                  // Hide the combobox
         }
 
         private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -167,36 +163,23 @@ namespace DBCViewer
 
             switch (column)
             {
-                case 0:                                     // name column without combobox
-                    item.BeginEdit();
-                    return;
-                case 1:
+                case 0: // index
+                    break;
+                case 1: // name
+                    ShowFakeControl(textBox2);
+                    break;
+                case 2: // type
                     comboBox1.Items.Clear();
                     comboBox1.Items.AddRange(comboBoxItems1);
+                    ShowFakeControl(comboBox1);
                     break;
-                case 2:
+                case 3: // isIndex
                     comboBox1.Items.Clear();
                     comboBox1.Items.AddRange(comboBoxItems2);
+                    ShowFakeControl(comboBox1);
                     break;
                 default:
                     break;
-            }
-
-            if (m_listViewSubItem != null)                  // Check if an actual item was clicked
-            {
-                Rectangle ClickedItem = m_listViewSubItem.Bounds;   // Get the bounds of the item clicked
-
-                // Adjust the top and left of the control
-                ClickedItem.X += listView1.Left;
-                ClickedItem.Y += listView1.Top;
-
-                comboBox1.Bounds = ClickedItem;             // Set combobox bounds to match calculation
-
-                comboBox1.Text = m_listViewSubItem.Text;    // Set the default text for the combobox to be the clicked item's text
-
-                comboBox1.Visible = true;                   // Show the combobox
-                comboBox1.BringToFront();                   // Make sure it is on top
-                comboBox1.Focus();                          // Give focus to the combobox
             }
         }
 
@@ -215,17 +198,35 @@ namespace DBCViewer
             listView1.Items[index].Selected = true;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listView1.SelectedItems.Count > 0)
-                listView1.Items.Insert(listView1.SelectedItems[0].Index + 1, new ListViewItem(new string[] { "newField", "int", "False" }));
-            else
-                listView1.Items.Add(new ListViewItem(new string[] { "newField", "int", "False" }));
+            for (var i = 0; i < listView1.Items.Count; ++i) // reorder index column
+            {
+                listView1.Items[i].SubItems[0].Text = i.ToString();
+            }
         }
 
-        private void DefinitionEditor_Load(object sender, EventArgs e)
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
         {
-            InitDefinitions();
+            m_listViewSubItem.Text = comboBox1.Text;        // Set text of ListView item
+
+            comboBox1.Hide();                               // Hide the combobox
+        }
+
+        private void comboBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)                   // Check if user pressed "ESC"
+                comboBox1.Hide();                           // Hide the combobox
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListViewItem item;
+            if (listView1.SelectedItems.Count > 0)
+                item = listView1.Items.Insert(listView1.SelectedItems[0].Index + 1, new ListViewItem(new string[] { "0", "newField", "int", "False" }));
+            else
+                item = listView1.Items.Add(new ListViewItem(new string[] { "0", "newField", "int", "False" }));
+            item.Selected = true;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -237,6 +238,40 @@ namespace DBCViewer
                 listView1.Items.Insert(index, cloned);
                 listView1.Items[index + 1].Selected = true;
                 listView1.Select();
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                m_listViewSubItem.Text = textBox2.Text;
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Escape)
+                textBox2.Hide();
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            m_listViewSubItem.Text = textBox2.Text;
+            textBox2.Hide();
+        }
+
+        private void ShowFakeControl(Control control)
+        {
+            if (m_listViewSubItem != null)                  // Check if an actual item was clicked
+            {
+                var ClickedItem = m_listViewSubItem.Bounds; // Get the bounds of the item clicked
+
+                // Adjust the top and left of the control
+                ClickedItem.X += listView1.Left;
+                ClickedItem.Y += listView1.Top;
+
+                control.Bounds = ClickedItem;               // Set Control bounds to match calculation
+
+                control.Text = m_listViewSubItem.Text;      // Set the default text for the Control to be the clicked item's text
+
+                control.Show();                             // Show the Control
+                control.BringToFront();                     // Make sure it is on top
+                control.Focus();                            // Give focus to the Control
             }
         }
     }
