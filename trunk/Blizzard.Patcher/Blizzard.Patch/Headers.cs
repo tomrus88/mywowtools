52a7538937d6a9b6a43d7878e57e16dbe97f83b4
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2010-10-20 06:42:05 +0000

    - Blizzard.Patcher: Initial release.

---
+++ trunk/Blizzard.Patcher/Blizzard.Patch/Headers.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Blizzard
+{
+    struct PTCH
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] m_magic;
+        public uint m_patchSize;
+        public uint m_sizeBefore;
+        public uint m_sizeAfter;
+    }
+
+    struct MD5_
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] m_magic;
+        public uint m_md5BlockSize;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] m_md5Before;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] m_md5After;
+    }
+
+    struct XFRM
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] m_magic;
+        public uint m_xfrmBlockSize;
+    }
+
+    struct BSDIFF40
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public byte[] m_magic;
+        public ulong m_ctrlBlockSize;
+        public ulong m_diffBlockSize;
+        public ulong m_sizeAfter;
+    }
+}
