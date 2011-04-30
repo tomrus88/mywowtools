302dfaf317227af8555def41483ca6147e78e642
tomrus88 <tomrus88@65da6843-c32c-0410-bcab-a998ee0685c5>
2011-04-30 05:28:53 +0000

    - Added real StormLib example.

--- trunk/StormLib/StormLibExample/Mpq.cs
+++ trunk/StormLib/StormLibExample/Mpq.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using StormLib;
 
 namespace Test
@@ -26,16 +27,8 @@ namespace Test
             archive.AddArchives(archiveNames);
         }
 
-        public static bool ExtractFile(string from, string to, OpenFileFlags dwSearchScope)
+        public static bool ExtractFile(string from, string to, OpenFile dwSearchScope)
         {
-            if (!archive.HasFile(from))
-                return false;
-
-            var dir = Path.GetDirectoryName(to);
-
-            if (!Directory.Exists(dir) && !String.IsNullOrEmpty(dir))
-                Directory.CreateDirectory(dir);
-
             return archive.ExtractFile(from, to, dwSearchScope);
         }
 
